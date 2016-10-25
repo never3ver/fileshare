d7be40bbfa5840d76ce7a48ba6ba70c4e81272ec
never3ver <a13xit@gmail.com>
2016-10-25 12:31:14 +0300

    getid3 with composer

--- app/Helper.php
+++ app/Helper.php
@@ -8,9 +8,17 @@ class Helper {
                 . 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
                 . '0123456789');
         for ($i = 0; $i < 45; $i++) {
-            $result .= $source[mt_rand(0, count($source) - 1)];
+//            $result .= $source[mt_rand(0, count($source) - 1)];
+            $result .= $source[array_rand($source)];
         }
         return $result;
     }
 
+    public static function getImagePath($tmpName) {
+        return "../files/" . $tmpName;
+    }
+
+    public static function getFilePath($tmpName) {
+        return "files/" . $tmpName;
+    }
 }
--- public/index.php
+++ public/index.php
@@ -47,25 +47,25 @@ $app->post('/', function (Request $request, Response $response) {
 
         $file->setTmpName($tmpName);
 
-        if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], "files/$tmpName")) {
+        if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], Helper::getFilePath($file->getTmpName()))) {
             $dataGateway->addFile($file);
             $url = $this->router->pathFor('list');
             return $response->withStatus(302)->withHeader('Location', $url);
-        } else {
-            $error = $_FILES['uploadfile']['error'];
-            $response = $this->view->render($response, "uploadError.html.twig", ["error"=>$error]);
-            return $response;
         }
     }
 });
 
 $app->get('/file/{id}', function (Request $request, Response $response, $args) {
-    require_once('../app/getid3/getid3.php');
     $id = (int) $args['id'];
     $dataGateway = new FileDataGateway($this->db);
     $file = $dataGateway->getFile($id);
     $getId3 = new getID3();
-    $fileInfo = $getId3->Analyze("files/{$file->getTmpName()}");
+    $fileInfo = $getId3->Analyze(Helper::getFilePath($file->getTmpName()));
+    if ($file->isImage()) {
+        $imagePath = Helper::getImagePath($file->getTmpName());
+        $response = $this->view->render($response, "file.html.twig", ["file" => $file, "fileInfo" => $fileInfo, "imagePath" => $imagePath]);
+        return $response;
+    }
     $response = $this->view->render($response, "file.html.twig", ["file" => $file, "fileInfo" => $fileInfo]);
     return $response;
 })->setName('file');
@@ -74,10 +74,10 @@ $app->get('/download/{id}', function (Request $request, Response $response, $arg
     $id = (int) $args['id'];
     $dataGateway = new FileDataGateway($this->db);
     $file = $dataGateway->getFile($id);
-    $path = "files/{$file->getTmpName()}";
-    $fh = fopen($path, "rb");
-    $stream = new \Slim\Http\Stream($fh); // create a new stream instance for the response body
-    if (file_exists($path)) {
+    $path = Helper::getFilePath($file->getTmpName());
+    if (is_readable($path)) {
+        $fh = fopen($path, "rb");
+        $stream = new \Slim\Http\Stream($fh); // create a new stream instance for the response body
         $response = $response->withHeader('Content-Type', $file->getType());
         $response = $response->withHeader('Content-Description', 'File Transfer');
         $response = $response->withHeader('Content-Disposition', 'attachment; filename=' . $file->getName());
@@ -93,7 +93,7 @@ $app->get('/download/{id}', function (Request $request, Response $response, $arg
 
 $app->get('/list', function (Request $request, Response $response) {
     $dataGateway = new FileDataGateway($this->db);
-    //getting latest 100 files
+//getting latest 100 files
     $files = $dataGateway->getAllFiles(100, 0);
     $response = $this->view->render($response, "list.html.twig", ["files" => $files]);
     return $response;
